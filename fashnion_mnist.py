5547837eee89c1964d7b17b707fda8338696dbbd
Maciej-Grzesik <mackeplz61@gmail.com>
2023-12-08 12:07:41 +0100

    Updated with myCallback class that stops training when accuracy reaches over 0.95

--- fashnion_mnist.py
+++ fashnion_mnist.py
@@ -1,5 +1,16 @@
 import tensorflow as tf
 import keras
+
+
+class myCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        if logs.get('accuracy') > 0.95:
+            self.model.stop_training = True
+
+
+callback = myCallback()
 data = keras.datasets.fashion_mnist
 
 (training_images, training_labels), (test_images, test_labels) = data.load_data()
@@ -8,13 +19,16 @@ training_images = training_images / 255.0
 test_images = test_images / 255.0
 
 model = keras.models.Sequential([
-    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Flatten(),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
-    ])
+])
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=50, callbacks=[callback])
+
+classifications = model.predict(test_images)
+print(classifications[0], test_labels[0])
