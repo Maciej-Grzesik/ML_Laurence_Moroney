4cbfaec63089be794fc750f6da41070a00c871c4
Maciej <mackeplz61@gmail.com>
2023-12-11 21:51:28 +0100

    Created horses-or-human.py machine learning algorithm that differentiates humans and horses

---
+++ horses-or-human.py
@@ -0,0 +1,76 @@
+import os
+import keras
+import numpy as np
+import keras.utils as image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.optimizers import RMSprop
+
+training_dir = r'horse-or-human-dataset\training_set'
+validation_dir = r'horse-or-human-dataset\validation_set'
+
+# Scaling of images to value 1/255
+train_datagen = ImageDataGenerator(rescale=1 / 255)
+validation_datagen = ImageDataGenerator(rescale=1 / 255)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(300, 300),
+    class_mode='binary'
+)
+
+train_generator = train_datagen.flow_from_directory(
+    training_dir,
+    target_size=(300, 300),
+    class_mode='binary'
+)
+
+model = keras.models.Sequential([
+    Conv2D(16, (3, 3), activation='relu', input_shape=(300, 300, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(32, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Flatten(),
+    Dense(512, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+# model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['accuracy'])
+
+history = model.fit_generator(
+    train_generator,
+    epochs=15,
+    validation_data=validation_generator
+)
+
+
+# Testing trained model
+def test_model(file_path):
+    for fn in os.listdir(file_path):
+        img = image.load_img(file_path + fn, target_size=(300, 300))
+
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+
+        images = np.vstack([x])
+        classes = model.predict(images, batch_size=10)
+        print(classes)
+        print(classes[0])
+        if classes[0] > 0.5:
+            print(fn + "shows a human")
+        else:
+            print(fn + "shows a horse")
+
+
+directory = r'horse-or-human-dataset/test_images/'
+test_model(directory)
