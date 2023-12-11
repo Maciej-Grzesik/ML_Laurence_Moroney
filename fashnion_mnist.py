750cda164d51894b3906547bdc007c94c468dc75
Maciej <mackeplz61@gmail.com>
2023-12-11 21:50:07 +0100

    Updated fashion_mnist.py with convltion layers for faster accuracy

--- fashnion_mnist.py
+++ fashnion_mnist.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import keras
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 
 class myCallback(keras.callbacks.Callback):
@@ -15,13 +16,20 @@ data = keras.datasets.fashion_mnist
 
 (training_images, training_labels), (test_images, test_labels) = data.load_data()
 
+training_images = training_images.reshape(60000, 28, 28, 1)
 training_images = training_images / 255.0
+
+test_images = test_images.reshape(10000, 28, 28, 1)
 test_images = test_images / 255.0
 
 model = keras.models.Sequential([
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Flatten(),
+    Dense(128, activation=tf.nn.relu),
+    Dense(10, activation=tf.nn.softmax)
 ])
 
 model.compile(optimizer='adam',
@@ -29,6 +37,9 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.fit(training_images, training_labels, epochs=50, callbacks=[callback])
+
+model.evaluate(test_images, test_labels)  # evaluates trained model  using validation data and returns loss value and
+# metrics values for the model
 
 classifications = model.predict(test_images)
 print(classifications[0], test_labels[0])
